b6ba6768a4f72bde04174cbeed7bf376ec1f672e
Cloris2015 <luckycloris@gmail.com>
2015-03-10 22:18:52 -0400

    add more views

--- FFC/App_Start/BundleConfig.cs
+++ FFC/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace FFC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js", "~/Scripts/jquery-1.11.3.jquery-ui.js", "~/Scripts/jquery/jquery-1.10.2.js", "~/Scripts/jquery/jquery.dataTables.min.js"));
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/jquery-1.11.3.jquery-ui.js", "~/Scripts/jquery-1.10.2.js","~/Scripts/common.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
